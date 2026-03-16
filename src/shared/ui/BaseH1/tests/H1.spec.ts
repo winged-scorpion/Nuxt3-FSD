1f3e100f0c9992d9e695b6ba22c956600bac74fd
lmb@one-touch.ru <lmb@git.one-touch.ru>
2026-03-16 11:53:07 +0400

    тесты

---
+++ src/shared/ui/BaseH1/tests/H1.spec.ts
@@ -0,0 +1,7 @@
+import { it } from 'vitest'
+
+import BaseH1 from '../ui/H1.vue' // замените на путь к вашему компоненту
+
+it('renders primary button by default', () => {
+  console.log('start', BaseH1)
+})
